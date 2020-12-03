758a5833b1bb679f981d09ddb1f43482ee983d4d
HenryS95 <71974053+HenryS95@users.noreply.github.com>
2020-12-03 14:39:55 +0000

    commit 2

---
+++ P3D/src/Assignment - Programming For 3D/Assets/Scripts/PickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField] private Transform theDest;
+
+    void OnMouseDown()
+
+
+    {
+        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+    }
+
+    void OnMouseUp()
+   
+     {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<SphereCollider>().enabled = true;
+    }
+
+}
